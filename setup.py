1619d21622d30f7de72d807eadbf92869c1e8797
uroojey <urooj.headgirl@gmail.com>
2026-02-21 02:57:55 +0530

    Logging and Exception

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ This module handles the packaging and distribution of the project.
 from setuptools import find_packages, setup
 from typing import List
 
+HYPEN_E_DOT = "-e ."
+
 
 def get_requirements(file_path: str) -> List[str]:
     '''
@@ -12,6 +14,10 @@ def get_requirements(file_path: str) -> List[str]:
     requirements = []
     with open(file_path) as file_obj:
         requirements = file_obj.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    return requirements
 
 
 setup(
